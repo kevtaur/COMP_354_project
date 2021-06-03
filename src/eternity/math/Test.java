4a91fbe1fe7e59390d18b6114a1826ae33dff687
“Khoa <khoatrinh2108@gmail.com>
2021-06-02 23:03:21 -0400

    testing

--- src/eternity/math/Test.java
+++ src/eternity/math/Test.java
@@ -1,9 +1,10 @@
 package eternity.math;
 
-import eternity.exception.OutOfRangeException;
+import eternity.exception.OutOfRangeException;;
 
 public class Test {
 	public static void main(String[] args) {
+		System.out.println("testing123");
 		try {
 			System.out.println(Functions.arccosine(1));
 			System.out.println(Functions.arccosine(0.5));
