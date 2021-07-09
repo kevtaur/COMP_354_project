01cc5b765d1fad4f108644e3f52e1443f0a584a1
Athigan1 <asinna99@gmail.com>
2021-07-08 23:49:29 -0400

    Added log tests and fixed typos

--- src/eternity/math/FunctionsTest.java
+++ src/eternity/math/FunctionsTest.java
@@ -49,7 +49,7 @@ class FunctionsTest {
 					assertEquals(new BigDecimal(scenario.expectedResult).setScale(5, RoundingMode.DOWN).doubleValue(), 
 							new BigDecimal(Functions.arccosine(scenario.input)).setScale(5, RoundingMode.DOWN).doubleValue());
 				} catch (Exception e) {
-					fail("Unepexted exception thrown");
+					fail("Unexpected exception thrown");
 				}
 			}
 			
@@ -93,7 +93,7 @@ class FunctionsTest {
 				try {
 					assertEquals(scenario.expectedResult, (double) Functions.semifactorial(scenario.input));
 				} catch (Exception e) {
-					fail("Unepexted exception thrown");
+					fail("Unexpected exception thrown");
 				}
 			}
 			
@@ -126,19 +126,96 @@ class FunctionsTest {
 	}
 	*/
 	
-	/**
 	@Test
 	void testLogDouble() {
-		fail("Not yet implemented");
+		class Case {
+			String name;
+			double input;
+			double expectedResult;
+			Exception exception;
+			
+			public Case(String name, double input, double expectedResult, Exception exception) {
+				this.name = name;
+				this.input = input;
+				this.expectedResult = expectedResult;
+				this.exception = exception;
+			}
+		}
+		
+		Case cases[] = new Case[] {
+				new Case("Input = 0.5", 0.5, -0.30102, null),
+				new Case("Input = 7", 7, 0.84509, null),
+				new Case("Input = 10", 10, 1.0, null),
+				new Case("Input = 0", 0, 0, new OutOfRangeException()),
+				new Case("Input = -5", -5, 0, new OutOfRangeException()),
+		};
+		System.out.println("\nTest: Common Logarithm (Base 10)");
+		for (Case scenario : cases) {
+			System.out.println(scenario.name);
+			
+			if (scenario.exception == null) {
+				try {
+					assertEquals(new BigDecimal(scenario.expectedResult).setScale(5, RoundingMode.DOWN).doubleValue(), 
+							new BigDecimal(Functions.log(scenario.input)).setScale(5, RoundingMode.DOWN).doubleValue());
+				} catch (Exception e) {
+					fail("Unexpected exception thrown");
+				}
+			}
+			
+			else {
+				Throwable exception = assertThrows(scenario.exception.getClass(), () -> Functions.log(scenario.input));
+				assertEquals(scenario.exception.getMessage(), exception.getMessage());
+			}
+		}
 	}
-	*/
 	
-	/**
 	@Test
 	void testLogDoubleDouble() {
-		fail("Not yet implemented");
+		class Case {
+			String name;
+			double input;
+			double base;
+			double expectedResult;
+			Exception exception;
+			
+			public Case(String name, double input, double base, double expectedResult, Exception exception) {
+				this.name = name;
+				this.input = input;
+				this.base = base;
+				this.expectedResult = expectedResult;
+				this.exception = exception;
+			}
+		}
+		
+		Case cases[] = new Case[] {
+				new Case("Input = 0.5, Base = 2", 0.5, 2, -1.0, null),
+				new Case("Input = 7, Base = 5", 7, 5, 1.20906, null),
+				new Case("Input = 50, Base = 78", 50, 78, 0.89793, null),
+				new Case("Input = -1, Base = 5", -1, 5, 0, new OutOfRangeException()),
+				new Case("Input = 3, Base = 1", 3, 1, 0, new OutOfRangeException()),
+				new Case("Input = 3, Base = 0", 3, 0, 0, new OutOfRangeException()),
+				new Case("Input = 3, Base = -5", 3, -5, 0, new OutOfRangeException()),
+		};
+		System.out.println("\nTest: Log (custom base)");
+		for (Case scenario : cases) {
+			System.out.println(scenario.name);
+			
+			if (scenario.exception == null) {
+				try {
+					assertEquals(new BigDecimal(scenario.expectedResult).setScale(5, RoundingMode.DOWN).doubleValue(), 
+							new BigDecimal(Functions.log(scenario.input, scenario.base)).setScale(5, RoundingMode.DOWN).doubleValue());
+				} catch (Exception e) {
+					fail("Unexpected exception thrown");
+				}
+			}
+			
+			else {
+				Throwable exception = assertThrows(scenario.exception.getClass(), () -> Functions.log(scenario.input, scenario.base));
+				assertEquals(scenario.exception.getMessage(), exception.getMessage());
+			}
+		}
 	}
-	*/
+	
 	
 	/**
 	@Test
