07a6b13208394ebcd86f2e5bee614e54d47896d8
Kevin Ve <kevinve123@gmail.com>
2021-07-16 13:05:55 -0400

    Update FunctionsTest.java

--- src/eternity/math/FunctionsTest.java
+++ src/eternity/math/FunctionsTest.java
@@ -3,6 +3,8 @@ package eternity.math;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import com.expression.parser.exception.CalculatorException;
 
 import org.junit.jupiter.api.DisplayName;
 
+import eternity.exception.EmptyInputException;
 import eternity.exception.OutOfRangeException;
 
 class FunctionsTest {
@@ -119,12 +122,44 @@ class FunctionsTest {
 	}
 	*/
 	
-	/**
 	@Test
 	void testMeanAbsoluteDeviation() {
-		fail("Not yet implemented");
+		class Case {
+			String name;
+			ArrayList<Double> input;
+			double expectedResult;
+			Exception exception;
+			
+			public Case(String name, ArrayList<Double> input, double expectedResult, Exception exception) {
+				this.name = name;
+				this.input = input;
+				this.expectedResult = expectedResult;
+				this.exception = exception;
+			}
+		}
+		Case cases[] = new Case[] {
+			new Case("Input = 0.0, 1.4", new ArrayList<Double>(Arrays.asList(0.0,1.4)), 0.7, null),
+			new Case("Input = -5.2, 3.2, 122.235, 12.0, -6.0, -1000000.9", new ArrayList<Double>(Arrays.asList(-5.2, 3.2, 122.235, 12.0, -6.0, -1000000.9)), 277785.04083333333, null),
+			new Case("Input = ", new ArrayList<Double>(), 0, new EmptyInputException("Empty input detected."))
+		};
+		
+		System.out.println("\nTest: Mean Absolute Deviation");
+		for (Case scenario : cases) {
+			System.out.println(scenario.name);
+			
+			if (scenario.exception == null) {
+				try {
+					assertEquals(scenario.expectedResult, (double) Functions.meanAbsoluteDeviation(scenario.input));
+				} catch (Exception e) {
+					fail("Unexpected exception thrown");
+				}
+			}
+			else {
+				Throwable exception = assertThrows(scenario.exception.getClass(), () -> Functions.meanAbsoluteDeviation(scenario.input));
+				assertEquals(scenario.exception.getMessage(), exception.getMessage());
+			}
+		}
 	}
-	*/
 	
 	@Test
 	void testLogDouble() {
